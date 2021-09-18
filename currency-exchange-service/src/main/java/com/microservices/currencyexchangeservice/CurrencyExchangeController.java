92ce131c7b4134b6597104c2e754fe3c3dc26a26
irmk323 <irmk323@yahoo.co.jp>
2021-09-18 17:31:33 +0100

    first commit

---
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,35 @@
+package com.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@Autowired
+	private CurrencyExchangeRepository repository;
+	
+	@Autowired
+	private Environment environment;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(
+			@PathVariable String from,
+			@PathVariable String to
+			) {
+		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+		if(currencyExchange == null) {
+			throw new RuntimeException("unable to find data for " + from + "and " + to );
+		}
+		String property = environment.getProperty("local.server.port");
+		currencyExchange.setEnvironment(property);
+		
+		return currencyExchange;
+		
+	}
+}
